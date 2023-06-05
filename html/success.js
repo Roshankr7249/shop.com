11241cfb62ae6209df031f5f1c6b703f366da149
Roshan_Kumar <mindsspiritual0@gmail.com>
2023-06-05 15:14:50 +0530

    new part

--- html/success.js
+++ html/success.js
@@ -1,5 +1,5 @@
 const redirectButton = document.getElementById("redirect-btn");
 
 redirectButton.addEventListener("click", () => {
-  window.location.href = "C:\Users\ACER\Dropbox\PC\Desktop\shop\shop.com\index.html";
+  window.location.href = "index.html";
 });
